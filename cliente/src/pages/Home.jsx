7171d3b19fb9c031382c316bac30323d1d350b67
sixzjkk <jenifergmporfirio@gmail.com>
2024-12-09 17:17:38 -0400

    ui 1

--- cliente/src/pages/Home.jsx
+++ cliente/src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import Registrar from "./Registro";
+import { jsPDF } from "jsPDF";
+import "jspdf-autotable";
+import { Button } from '@mui/material';
 
 export default function Home() {
 
@@ -29,19 +32,45 @@ export default function Home() {
     }
   }
 
+  const exportarPDF = () => {
+    const doc = new jsPDF();
+    const tabela = usuarios.map( usuario => [
+      usuario.id,
+      usuario.nome,
+      usuario.email
+    ]);
+    doc.text("Lista de Usuários", 10, 10);
+    doc.autoTable({
+      head: [["Id", "Nome", "Email"]],
+      body: tabela
+    });
+
+    doc.save("alunos.pdf");
+  }
+
   return (
-    <table>
-      <tr>
-        <td>Nome</td>
-        <td>E-mail</td>
-      </tr>
-      {usuarios.map((usuario) =>
-        <tr key={usuario.id}>
-          <td>{usuario.nome}</td>
-          <td>{usuario.email}</td>
-          <td><button onClick={() => remover(usuario.id)}>X</button></td>
-        </tr>
-      )}
-    </table>
+    <div>
+      <button onClick={()=> exportarPDF()}>Gerar PDF</button>
+      <table>
+        <thead>
+          <Button variant="contained" onClick={()
+            Gerar PDF
+            </Button>
+          <tr>
+            <td>Nome</td>
+            <td>E-mail</td>
+          </tr>
+        </thead>
+        <tbody>
+          {usuarios.map((usuario) =>
+            <tr key={usuario.id}>
+              <td>{usuario.nome}</td>
+              <td>{usuario.email}</td>
+              <td><button onClick={() => remover(usuario.id)}>X</button></td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 }
